1c972d2e30d9cf9edcb86d38a6324aedf71d181c
Daniel <georgeabiamakadaniel@gmail.com>
2026-03-05 11:26:16 +0100

    update

--- ecommerce-dashboard-template/src/lib/axios.js
+++ ecommerce-dashboard-template/src/lib/axios.js
@@ -1,6 +1,9 @@
-import axios from "axios";
+﻿import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:4000/api/v1",
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:4000/api/v1"
+      : import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
