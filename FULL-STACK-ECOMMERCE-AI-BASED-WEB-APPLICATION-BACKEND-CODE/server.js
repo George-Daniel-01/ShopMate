3509fcf0879452bb0af09036104c476373fb6d10
Daniel <georgeabiamakadaniel@gmail.com>
2026-03-06 20:55:27 +0100

    fix server for vercel serverless

--- FULL-STACK-ECOMMERCE-AI-BASED-WEB-APPLICATION-BACKEND-CODE/server.js
+++ FULL-STACK-ECOMMERCE-AI-BASED-WEB-APPLICATION-BACKEND-CODE/server.js
@@ -1,4 +1,4 @@
-import dotenv from "dotenv";
+﻿import dotenv from "dotenv";
 dotenv.config({ path: "./config/config.env" });
 
 import { connectDB } from "./database/db.js";
@@ -15,6 +15,4 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_CLIENT_SECRET,
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`🚀 Server running on port ${process.env.PORT}`);
-});
+export default app;
