be5fad8304ab8534fc4fb25cb4f8f884d0558e18
Daniel <georgeabiamakadaniel@gmail.com>
2026-03-06 19:59:44 +0100

    fix adminSlice baseURL for production

--- ecommerce-dashboard-template/src/lib/axios.js
+++ ecommerce-dashboard-template/src/lib/axios.js
@@ -1,9 +1,9 @@
 ﻿import axios from "axios";
 
-export const axiosInstance = axios.create({
-  baseURL:
-    import.meta.env.MODE === "development"
-      ? "http://localhost:4000/api/v1"
-      : import.meta.env.VITE_API_URL,
+
+const axiosInstance = axios.create({
+  baseURL: import.meta.env.MODE === "development"
+    ? "http://localhost:4000/api/v1"
+    : import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
