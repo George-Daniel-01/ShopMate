fdeee1eb5d00ff6dcbb2121695661636ae8d22ea
Daniel <georgeabiamakadaniel@gmail.com>
2026-03-07 11:52:08 +0100

    update

--- ecommerce-frontend-template/src/lib/axios.js
+++ ecommerce-frontend-template/src/lib/axios.js
@@ -4,7 +4,7 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:4000/api/v1"
-      : "/api/v1",
+      : import.meta.env.VITE_API_URL,  
   withCredentials: true,
 });
 
