4920c9adf1a82ffdd511d4215ec74fb8f383a72e
Daniel <georgeabiamakadaniel@gmail.com>
2026-03-07 19:40:52 +0100

    fix: use VITE_API_URL directly in axios

--- ecommerce-frontend-template/src/lib/axios.js
+++ ecommerce-frontend-template/src/lib/axios.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL:
-    import.meta.env.MODE === "development"
-      ? "http://localhost:4000/api/v1"
-      : import.meta.env.VITE_API_URL,  
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:4000/api/v1",
   withCredentials: true,
 });
 
