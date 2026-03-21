35215d06a75a117affae13ad3ef4265180274896
Daniel <georgeabiamakadaniel@gmail.com>
2026-03-21 19:36:17 +0100

    update

--- FULL-STACK-ECOMMERCE-AI-BASED-WEB-APPLICATION-BACKEND-CODE/server.ts
+++ FULL-STACK-ECOMMERCE-AI-BASED-WEB-APPLICATION-BACKEND-CODE/server.ts
@@ -6,18 +6,18 @@ import app from "./app.js";
 import { v2 as cloudinary } from "cloudinary";
 import { createTables } from "./utils/createTables.js";
 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLIENT_NAME,
+  api_key: process.env.CLOUDINARY_CLIENT_API,
+  api_secret: process.env.CLOUDINARY_CLIENT_SECRET,
+});
+
 const start = async (): Promise<void> => {
   await connectDB();
   await createTables();
 
-  cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLIENT_NAME,
-    api_key: process.env.CLOUDINARY_CLIENT_API,
-    api_secret: process.env.CLOUDINARY_CLIENT_SECRET,
-  });
-
-  app.listen(process.env.PORT, () => {
-    console.log("Server running on port " + process.env.PORT);
+  app.listen(process.env.PORT || 4000, () => {
+    console.log("Server running on port " + (process.env.PORT || 4000));
   });
 };
 
